666cd0e59218c7134753a5df82edfbf6a45a6b6e
Oualid <Oualid.b@hotmail.de>
2016-11-08 23:24:41 +0100

    Walou: Restart wenn ball ins loch faellt hinzugefuegt, Explosion wenn ball ins loch faellt hinzugefuegt

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject ball;
+    public float explosionRadius = 5.5f;
+    private Collider[] hitColliders;
+    public float explosionsPower = 10.0f;
+    public LayerMask expliosionLayers;
+    public float explosionUpPressure = 1.0f;
+
+    public void OnTriggerEnter(Collider enteredBall)
+    {
+        if(ball == enteredBall.gameObject)
+        {
+            Vector3 explosionPoint = ball.transform.position;
+
+            hitColliders = Physics.OverlapSphere(explosionPoint, this.explosionRadius);
+
+            foreach(Collider hitColl in hitColliders)
+            {
+                if (hitColl.GetComponent<Rigidbody>() != null)
+                {
+                    hitColl.GetComponent<Rigidbody>().isKinematic = false;
+                    hitColl.GetComponent<Rigidbody>().AddExplosionForce(explosionsPower, explosionPoint, explosionRadius, explosionUpPressure, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour {
+
+    public GameObject ball;
+
+    private void OnTriggerEnter(Collider enteredObj)
+    {
+        
+        if (ball == enteredObj.gameObject)
+        {
+
+            //yield return new WaitForSecondsRealtime(5);
+            StartCoroutine(Delay());
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+
+    }
+}
