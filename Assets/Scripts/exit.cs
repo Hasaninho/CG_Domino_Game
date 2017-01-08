17bad69fd6499f0b6838ba0204fa55d0e3a6b5e3
Oualid <Oualid.b@hotmail.de>
2017-01-08 18:05:54 +0100

    Walou: Windmill texture hinzugefuegt, Portal Texture hinzugefuegt, Exit Button im Menue hinzugefuegt LoadButton entfernt, exit script geschrieben, level portale aktualisiert, exit script zu allen Level hinzugefuegt

---
+++ Assets/Scripts/exit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exit : MonoBehaviour
+{
+    public void exitMethod()
+	{
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
